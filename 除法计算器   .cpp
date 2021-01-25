98b627d30147d3f2400da790e476feb8dd0bc155
xxxx123123 <65841325+xxxx123123@users.noreply.github.com>
2021-01-25 23:08:32 +0800

    Add files via upload

---
+++ 除法计算器   .cpp
@@ -0,0 +1,15 @@
+#include<iostream>                            
+using namespace std;
+int main()
+{
+int a=0;
+int b=0;
+int s=0;   
+cout<<"这里是除法计算器，请输入第一个数"<<endl;
+cin>>a;
+cout<<"接下来是第二个数" <<endl;
+cin>>b;
+s=a/b;
+cout<<"答案是"<<s;
+	return 0;                             
+}            
